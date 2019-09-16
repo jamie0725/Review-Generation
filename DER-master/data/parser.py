9f8a09a016b5523b3a7f657797bd12a825b4a94d
ErikStammes <erik.stammes@student.uva.nl>
2019-09-16 16:03:17 +0200

    Updated parser notebook and exported it back to regular python file

--- DER-master/data/parser.py
+++ DER-master/data/parser.py
@@ -1,70 +1,176 @@
+# In[1]:
+
+
 import json
+import csv
 import pickle
+import re
 import numpy as np
-
 from collections import defaultdict
-
-# NOTE: You need at least Python 3.6 to run this code
-
-def string_to_word_ids(id_word_dict, string):
-    # Maps a string (sentence) to word IDs as defined in the id_word_dict parameter
-    wordids = []
-    for word in string.split():
-        if word.lower() in id_word_dict:
-            wordids.append(str(id_word_dict[word.lower()]))
-    return wordids
-
 filename_input = 'reviews_Automotive_5.json'
-filename_output = 'train_ided_whole_data'
-id_word_dict_path = 'id_word_dict'
-
-# Configurable window length (the paper proposes a value between 2 and 5)
-WINDOW_LENGTH = 2
-
 # Load the raw data
 raw_data = [json.loads(line) for line in open(filename_input, 'r')]
-id_word_dict = pickle.load(open(id_word_dict_path, 'rb'))
-word_id_dict = {v: k for k, v in id_word_dict.items()}
 
-# Initialize dictionaries
-user_to_reviews = defaultdict(list)
-reviews = dict()
 
+# In[2]:
+
+
+# Generate id_user_dict/id_item_dict/id_word_dict
+users = set()
+items = set()
+vocab = set()
+max_interaction_length = 0
 for line in raw_data:
-    # Map user IDs to review IDs
-    user_to_reviews[line['reviewerID']].append(line['asin'])
-    # Save reviews by ID for quick access
-    reviews[line['asin']] = line
+    UserID = line['reviewerID']
+    ItemID = line['asin']
+    sentence = re.sub(r'[^\w\s]|\d','',line['reviewText'].lower())
+    words = [word for word in  sentence.split()]
+    if len(words) > max_interaction_length:
+        max_interaction_length = len(words)
+    users.add(UserID)
+    items.add(ItemID)
+    for word in words:
+        vocab.add(word)
+id_user_dict = dict(zip(range(len(users)), users))
+id_item_dict = dict(zip(range(len(items)), items))
+id_word_dict = dict(zip(range(len(vocab)), vocab))
+word_id_dict = dict(zip(vocab,range(len(vocab))))
+with open('id_user_dict', 'wb') as f:
+    pickle.dump(id_user_dict, f)
+with open('id_item_dict', 'wb') as f:
+    pickle.dump(id_item_dict, f)
+with open('id_word_dict', 'wb') as f:
+    pickle.dump(id_word_dict, f)
 
-result = []
-for user in user_to_reviews:
-    # Get all reviews by user
-    user_reviews = [reviews[review_id] for review_id in  user_to_reviews[user]]
-    for review in user_reviews:
-        # Select only reviews that are written before the current one
-        prev_reviews = [r for r in user_reviews if r['unixReviewTime'] < review['unixReviewTime']]
-        if len(prev_reviews) < WINDOW_LENGTH:
-            # Not enough older reviews
-            continue
-        # Select reviews at random when there are too many. #TODO: this can be improved
-        prev_reviews = np.random.choice(prev_reviews, WINDOW_LENGTH)
-        # Start formatting the row
-        row = f"{user}&&"
-        for i, r in enumerate(prev_reviews):
-            wordids = string_to_word_ids(word_id_dict, r['reviewText'])
-            # Format row
-            words = '::'.join(wordids)
-            row += f"{r['asin']}||{r['overall']}||{words}||{r['unixReviewTime']}"
-            if i < len(prev_reviews)-1:
-                # Only added between previous reviews
-                row += '()'
-        wordids = string_to_word_ids(word_id_dict, r['reviewText'])
-        # Finish formatting row and store it      
-        words = '::'.join(wordids)
-        row += f"&&{review['asin']}||{r['overall']}||{words}||{r['unixReviewTime']}"
-        result.append(row)
+
+# In[3]:
+
+
+print('length of id_user_dict:',len(id_user_dict))
+print('length of id_item_dict:',len(id_item_dict))
+print('length of id_word_dict:',len(id_word_dict))
+
+
+# In[4]:
+
+
+def get_key_by_value(value,D):
+    id = list(D.values()).index(value)
+    return list(D.keys())[id]
+
+
+# In[5]:
+
+
+"""
+item_reviews
+    Key: ItemID 
+    Value: All the user reviews received by the Item
+user_item_review
+    Key: UserID@ItemID 
+    Value: review from the user to the item
+user_purchased_items
+    Key: UserID
+    Value: list of purchasedID
+"""
+item_real_reviews = defaultdict(list)
+user_item_review = defaultdict(list)
+user_purchased_items = defaultdict(list)
+for line in raw_data:
+    # Turn the original ID into the index in the dictionary
+    UserID = get_key_by_value(line['reviewerID'], id_user_dict)
+    ItemID = get_key_by_value(line['asin'], id_item_dict)
+    item_real_reviews[ItemID].append(line)
+    UserItem = '{}@{}'.format(UserID,ItemID)
+    user_item_review[UserItem].append(line)
+    user_purchased_items[UserID].append(ItemID)
+with open('item_real_reviews', 'wb') as f:
+    pickle.dump(item_real_reviews, f)
+with open('item_reviews', 'wb') as f:
+    pickle.dump(item_real_reviews, f)
+with open('user_item_review', 'wb') as f:
+    pickle.dump(user_item_review, f)
+with open('train_user_purchased_items', 'wb') as f:
+    pickle.dump(user_purchased_items, f)    
+with open('validation_user_purchased_items', 'wb') as f:
+    pickle.dump(user_purchased_items, f)        
+with open('test_user_purchased_items', 'wb') as f:
+    pickle.dump(user_purchased_items, f)        
+
+
+# In[6]:
+
+
+def parseStr(review):
+    ItemID = get_key_by_value(review['asin'], id_item_dict)
+    rating = review['overall']
+    time = review['unixReviewTime']
+    word_ids = []
+    review_text = re.sub(r'[^\w\s]|\d','',review['reviewText'].lower())
+    for word in review_text.split():
+        word_id = word_id_dict[word]
+        word_ids.append(str(word_id)) 
+    return '{}||{:1}||'.format(ItemID,rating)+'::'.join(word_ids)+"||{}".format(time)
+
+train = []
+validation = []
+test = []
+for UserID in range(len(id_user_dict)):
+    items = user_purchased_items[UserID]
+    if UserID % 100 == 0:
+        print('Processing {}/{}'.format(UserID,len(id_user_dict)))
+    reviews = []
+    for ItemID in items:
+        UserItem = '{}@{}'.format(UserID,ItemID)
+        reviews.append(user_item_review[UserItem][0])
+    reviews_sorted = sorted(reviews, key=lambda k: k['unixReviewTime']) 
+    for i in range(2,len(reviews_sorted)):
+        target_review = reviews_sorted[i]
+        pre_review = reviews_sorted[i-1]
+        pre_pre_review = reviews_sorted[i-2]
+        row = '{}&&'.format(UserID)+parseStr(pre_review)+'()'+parseStr(pre_pre_review)+'&&'+parseStr(target_review)
+        if i < len(reviews_sorted) - 2:
+            train.append(row)
+        elif i == len(reviews_sorted) - 2:
+            test.append(row)
+        elif i == len(reviews_sorted) - 1:
+            validation.append(row)
+print('Done!')
+
+
+# In[7]:
+
 
 # Save output
-with open(filename_output, 'w') as out_file:
-    out_file.write('\n'.join(result))
+with open('train_ided_whole_data', 'w') as out_file:
+    out_file.write('\n'.join(train))
+with open('validation_ided_whole_data', 'w') as out_file:
+    out_file.write('\n'.join(validation))    
+with open('test_ided_whole_data', 'w') as out_file:
+    out_file.write('\n'.join(test))
+with open('train_validation_ided_whole_data', 'w') as out_file:
+    out_file.write('\n'.join(train+validation)) 
+
+
+# In[9]:
+
+
+data_statistics = {
+    'max_interaction_length': max_interaction_length,
+    'interaction_num': len(raw_data),
+    'max_sentence_length': 1,
+    'max_sentence_word_length': max_interaction_length,
+    'time_bin_number': 1,
+    'user_num': len(id_user_dict),
+    'item_num': len(id_item_dict),
+    'word_num': len(id_word_dict)
+}
+with open('data_statistics', 'wb') as f:
+    pickle.dump(data_statistics, f)
+
+
+# In[ ]:
+
+
+
 
